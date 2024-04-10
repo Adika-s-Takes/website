ec739974a1a70fc846ef93e8913de17d1a86c3aa
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2024-04-10 12:23:42 +0100

    Updates to Item disply ADMIN

--- products/admin.py
+++ products/admin.py
@@ -10,7 +10,7 @@ class ProductDetailAdmin(admin.TabularInline):
 
 class ItemAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin, ProductDetailAdmin]
-    list_display = ['name', 'id', 'price', 'type', 'stock', 'active']
+    list_display = ['name', 'id', 'price', 'initial_price', 'type', 'stock', 'active', 'custom_item_id', 'kit_type', 'customizable', 'product_tags', 'sizes']
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'item_name', 'quantity', 'price', 'customer', 'status', 'shipping_info_address', 'customer_phone_number', 'customer_email', 'paid']
