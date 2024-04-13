014704fed4759dbf2bdd856d45d81316a0f42c9a
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2024-04-13 19:39:41 +0100

    Added Right Middleware this time

--- adikastakes/settings.py
+++ adikastakes/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
