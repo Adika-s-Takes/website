d39c6cf8df1adaf227fbb206fda0edcdf10f3c94
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2024-04-13 14:19:03 +0100

    Changed Call Method in URLS and HTML file for wishlist

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("review/<str:pk>", views.review, name="review"),
     path('search', views.item_search_results, name="search"),
     path('wishlist', views.wishlist, name="wishlist"),
-    path('wishlist/add/<str:product_name>/', views.add_to_wishlist, name='add_to_wishlist'),
-    path('wishlist/remove/<int:wishlist_item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('wishlist/add/<str:pk>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove/<str:pk>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
